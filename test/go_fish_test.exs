655a7b2908b9f1393a4b062b26ca9c4ebfd22ecf
simonelnahas <simonelnahas@gmail.com>
2022-07-20 15:37:56 +0200

    WIP

--- test/go_fish_test.exs
+++ test/go_fish_test.exs
@@ -1,23 +1,22 @@
 defmodule GoFishTest do
   use ExUnit.Case
 
-  # setup do
-  #   # assert {:ok,_} = GoFish.Ocean.start_link(:sorted)
-  #   # assert {:ok,_} = GoFish.Player.start_link({:simon, false})
-  #   # assert {:ok,_} = GoFish.Player.start_link({:john, true})
-  #   # on_exit(fn ->
-  #   #   GoFish.Player.stop(:simon)
-  #   #   GoFish.Player.stop(:john)
-  #   #   :timer.sleep(1000)
-  #   #   end)
-  # end
+  setup do
+    assert {:ok,_} = GoFish.Ocean.start_link(:sorted)
+    assert {:ok,_} = GoFish.Player.start_link({:simon, false})
+    assert {:ok,_} = GoFish.Player.start_link({:john, true})
+    on_exit(fn ->
+      GoFish.Player.stop(:simon)
+      GoFish.Player.stop(:john)
+      :timer.sleep(5000)
+      end)
+  end
 
-  start_supervised([
-    # Starts a worker by calling: GoFish.Worker.start_link(arg)
-    {GoFish.Ocean, []},
-    Supervisor.child_spec({GoFish.Player, {:john, true}}, id: :john),
-    Supervisor.child_spec({GoFish.Player, {:simon, false}}, id: :simon)
-  ])
+  # setup do
+  #   start_supervised!(GoFish.Ocean)
+  #   start_supervised!(Supervisor.child_spec({GoFish.Player, {:john, true}}, id: :john))
+  #   start_supervised!(Supervisor.child_spec({GoFish.Player, {:simon, true}}, id: :simon))
+  # end
 
   test "take card on empty hands" do
     assert :went_fishing == GoFish.Player.take_all_your(3, :john, :simon)
