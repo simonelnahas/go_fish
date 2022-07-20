0d6e04260b026914c51fe1ff4905d6709b097f80
Simon El Nahas <simon.el.nahas@gmail.com>
2022-07-20 13:44:52 +0200

    adding books feature (#3)
    Co-authored-by: simonelnahas <simonelnahas@gmail.com>
    Co-authored-by: john.holt@erlang-solutions.com <john.holt@erlang-solutions.com>
    Co-authored-by: rhysdavey <rhysdavey@users.noreply.github.com>

--- lib/go_fish/player.ex
+++ lib/go_fish/player.ex
@@ -33,21 +33,15 @@ defmodule GoFish.Player do
 
     # Helper functions
 
-    def take_card_from_ocean(state, card) do
-      IO.puts("I drew the card #{inspect(card)} from the ocean")
-      newState = %{state |
-                    :isMyTurn => false,
-                    :hand => [card | Map.get(state, :hand)]}
-      {:reply, :went_fishing, newState}
-    end
-
     def go_fish(state) do
       IO.puts("They didn't have the requested value so I go fishing")
       case GoFish.Ocean.take_card() do
-          {:card, card} -> take_card_from_ocean(state, card)
+          {:card, card} ->
+            IO.puts("I drew the card #{inspect(card)} from the ocean")
+            {:reply, :went_fishing, %{add_card(state,card) | :isMyTurn => false}}
           :no_cards_left ->
             IO.puts("There are no cards left in the ocean")
-            {:reply, :game_over, state}
+            {:reply, :no_cards_left, state}
       end
     end
 
@@ -63,17 +57,63 @@ defmodule GoFish.Player do
   def init(true) do
     {:ok,
       # initial state:
-      %{:hand => [], :isMyTurn => true}}
+      %{:hand => [], :isMyTurn => true, :books => []}}
   end
 
   def init(false) do
     {:ok,
       # initial state:
-      %{:hand => [], :isMyTurn => false}}
+      %{:hand => [], :isMyTurn => false, :books => []}}
+      #TODO: starting removing :hand and :books from initial state and then
+      # set them when used the first time
   end
 
   def handle_cast(:stop, _state) do
     {:stop, :normal}
+  end
+
+  def get_matches_from_hand(hand, value) do
+    case Enum.group_by(hand, fn card -> card.value == value end) do
+      %{true => matches, false => new_hand} ->
+        %{:matches => matches, :new_hand => new_hand}
+      %{false => new_hand}  ->
+        %{:matches => [], :new_hand => new_hand}
+      %{true => matches}  ->
+        %{:matches => matches, :new_hand => []}
+      %{} ->
+        %{:matches => [], :new_hand => []}
+    end
+  end
+
+  def get_books(cards) do
+    cards
+      |> Enum.group_by(& &1.value)
+      |> Map.to_list()
+      |> Enum.filter(fn {_key, value} -> Enum.count(value) == 4 end)
+      |> Enum.map(fn {key, _value} -> key end)
+  end
+
+  def create_books(state) do
+    %{:hand => hand} = state
+    books = get_books(hand)
+    if books != [] do
+      IO.puts("collected books: #{hd(books)}")
+    end
+    state_with_books = %{state | :books => books ++ Map.get(state,:books)}
+    List.foldl(books, state_with_books,
+      fn book_value, s ->
+        %{:new_hand => new_hand} = get_matches_from_hand(Map.get(s, :hand), book_value)
+        %{s | :hand => new_hand}
+    end)
+  end
+
+  def add_card(state, card) do
+    %{state | :hand => [card | Map.get(state, :hand)]}
+     |> create_books()
+  end
+
+  def add_cards(state, cards) do
+    List.foldl(cards, state, & add_card(&2, &1))
   end
 
   def handle_call({:take_all_your, num, giver}, _from, state) do
@@ -90,20 +130,22 @@ defmodule GoFish.Player do
     end
   end
 
-  def handle_call({:give_all_my, num}, _from, state) do
-    hand = Map.get(state, :hand)
-    matches = Enum.filter(hand,fn x -> x.value == num end)
-    new_hand = Enum.filter(hand,fn x -> x.value != num end)
-    case matches do
-      [] ->
+  def get_matches(hand, value, state) do
+    case get_matches_from_hand(hand, value) do
+      %{:matches => []} ->
         {:reply,
           :go_fish,
           %{state | :isMyTurn => true}}
-      matches  ->
+      %{:matches => matches, :new_hand => new_hand}  ->
         {:reply,
           {:matches, matches},
           %{state | :hand => new_hand}}
+
     end
+  end
+
+  def handle_call({:give_all_my, num}, _from, state) do
+    get_matches(Map.get(state, :hand), num, state)
   end
 
   def handle_call(:get_state, _from, state) do
@@ -120,7 +162,8 @@ defmodule GoFish.Player do
               :no_cards_left -> acc
             end
           end)
-        {:reply, :got_cards, %{state | :hand => [card1|cards] ++ Map.get(state, :hand)} }
+          # IO.puts(":got_cards #{inspect([card1|cards])}\n\n")
+        {:reply, :got_cards, add_cards(state, [card1|cards]) }
     end
   end
 
