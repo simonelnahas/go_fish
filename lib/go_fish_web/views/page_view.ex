fbc1e4a601c0b01d0b568cbaaac7e082470341de
simonelnahas <simonelnahas@gmail.com>
2022-08-03 09:30:42 +0200

    improved layout of ask for card

--- lib/go_fish_web/views/page_view.ex
+++ lib/go_fish_web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule GoFishWeb.PageView do
+  def uppercase(atom) when is_atom(atom), do: uppercase(to_string(atom))
+  def uppercase(<<first::utf8, rest::binary>>), do: String.upcase(<<first::utf8>>) <> rest
+
+
   use GoFishWeb, :view
 end
