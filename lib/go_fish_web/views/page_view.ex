c2cf73ddcf11b02c25e2eeddd9cd375fe9351b22
simonelnahas <simonelnahas@gmail.com>
2022-08-03 09:54:45 +0200

    displays cards

--- lib/go_fish_web/views/page_view.ex
+++ lib/go_fish_web/views/page_view.ex
@@ -2,6 +2,11 @@ defmodule GoFishWeb.PageView do
   def uppercase(atom) when is_atom(atom), do: uppercase(to_string(atom))
   def uppercase(<<first::utf8, rest::binary>>), do: String.upcase(<<first::utf8>>) <> rest
 
+  def suit_to_emoji(:spades), do: "♠️"
+  def suit_to_emoji(:diamonds), do: "♦️"
+  def suit_to_emoji(:clubs), do: "♣️"
+  def suit_to_emoji(:hearts), do: "♥️"
+
 
   use GoFishWeb, :view
 end
