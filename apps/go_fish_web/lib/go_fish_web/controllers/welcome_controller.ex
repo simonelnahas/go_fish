58a6877a9a1035acce509d51f71dd92618604832
Simon El Nahas <simon.el.nahas@gmail.com>
2022-08-17 10:15:28 +0200

    cleanup

--- apps/go_fish_web/lib/go_fish_web/controllers/welcome_controller.ex
+++ apps/go_fish_web/lib/go_fish_web/controllers/welcome_controller.ex
@@ -5,20 +5,4 @@ defmodule GoFishWeb.WelcomeController do
     render(conn, "index.html",[])
   end
 
-
-  # def draw_card(conn,%{"player" => player}) do
-  #   GoFish.Player.draw_cards(String.to_existing_atom(player), 1)
-  #   redirect(conn, to: "/")
-  # end
-
-  # def ask_for_card(conn, params) do
-  #   %{"taker" => taker, "giver" => giver, "value" => value_string} = conn.query_params
-  #   # values are fine
-  #   { value, _ } = Integer.parse(value_string)
-  #   IO.puts("value: #{value}")
-  #   GoFish.Player.take_all_your(value, String.to_existing_atom(taker), String.to_existing_atom(giver)) #TODO consider if we can make this more concise.
-  #   redirect(conn, to: "/")
-  # end
-
-
 end
