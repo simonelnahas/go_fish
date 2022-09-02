0c7ddca9c9da1c299b1f62d1d3ac63944c7c3000
Simon El Nahas <simon.el.nahas@gmail.com>
2022-09-02 17:20:31 +0200

    redirects to `/welcome` when uninitialized

--- apps/go_fish_web/lib/go_fish_web/controllers/game_controller.ex
+++ apps/go_fish_web/lib/go_fish_web/controllers/game_controller.ex
@@ -2,25 +2,30 @@ defmodule GoFishWeb.GameController do
   use GoFishWeb, :controller
 
   def index(conn, _params) do
-    players = GoFish.Controller.get_players()
-
-    player_states_list = Enum.reduce(players, [],
-      fn player, acc ->
+    if nil == Process.whereis(GoFish.Controller) do
+      redirect(conn, to: "/welcome")
+    else
+      players = GoFish.Controller.get_players()
+      player_states_list =
+      Enum.reduce(players, [], fn player, acc ->
         [{player, GoFish.Player.get_state(player)} | acc]
-    end)
+      end)
 
-    player_states = Map.new(player_states_list)
+      player_states = Map.new(player_states_list)
 
-    render(conn,
-      "index.html",
-      player_states: player_states)
+      render(
+        conn,
+        "index.html",
+        player_states: player_states
+        )
+    end
   end
 
   def string_to_atom_list(s) do
     s
     |> String.downcase()
     |> String.split(", ")
-    |> Enum.map(& String.to_atom(&1))
+    |> Enum.map(&String.to_atom(&1))
   end
 
   def start_game(conn, _params) do
@@ -29,10 +34,12 @@ defmodule GoFishWeb.GameController do
 
     DynamicSupervisor.start_child(GoFish.DynamicGameSupervisor, GoFish.Ocean)
     DynamicSupervisor.start_child(GoFish.DynamicGameSupervisor, GoFish.Controller)
+
     for player <- players do
       DynamicSupervisor.start_child(
         GoFish.DynamicGameSupervisor,
-        Supervisor.child_spec({GoFish.Player, {player, false}}, id: player))
+        Supervisor.child_spec({GoFish.Player, {player, false}}, id: player)
+      )
     end
 
     GoFish.Controller.start_game(players)
@@ -40,7 +47,7 @@ defmodule GoFishWeb.GameController do
     redirect(conn, to: "/")
   end
 
-  def draw_card(conn,%{"player" => player}) do
+  def draw_card(conn, %{"player" => player}) do
     GoFish.Player.draw_cards(String.to_existing_atom(player), 1)
     redirect(conn, to: "/")
   end
@@ -48,9 +55,15 @@ defmodule GoFishWeb.GameController do
   def ask_for_card(conn, _params) do
     %{"taker" => taker, "giver" => giver, "value" => value_string} = conn.query_params
     # values are fine
-    { value, _ } = Integer.parse(value_string)
+    {value, _} = Integer.parse(value_string)
     IO.puts("value: #{value}")
-    GoFish.Player.take_all_your(value, String.to_existing_atom(taker), String.to_existing_atom(giver)) #TODO consider if we can make this more concise.
+    # TODO consider if we can make this more concise.
+    GoFish.Player.take_all_your(
+      value,
+      String.to_existing_atom(taker),
+      String.to_existing_atom(giver)
+    )
+
     redirect(conn, to: "/")
   end
 end
